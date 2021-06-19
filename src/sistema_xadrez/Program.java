2cb02519959e1b4ceac59bdcd2ec12dda6786f2d
Rafael Viana <rafaelviana544@yahoo.com>
2021-06-19 18:23:15 -0300

    chess laver and priting the board

--- src/sistema_xadrez/Program.java
+++ src/sistema_xadrez/Program.java
@@ -1,14 +1,15 @@
 package sistema_xadrez;
 
-import boardgame.Position;
+import chess.ChessMatch;
+
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
-		
-		System.out.println(pos);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
